87bc3a5ada1207d71756195f669800b5a73129bb
fanfan123 <1820148513@qq.com>
2018-04-26 15:11:20 +0800

    taxi

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+function Charge(Input) {
+    function getPrice(Input) {
+        var distance = Input[0];
+        var time = Input[1];
+        var time_price = time * 0.25;
+        var dis_price;
+        var totalPrice;
+        var result;
+        if(distance<=2){
+            dis_price = 6;
+        }
+        if(distance>2 && distance<=8){
+            dis_price=6+(distance-2)*0.8;
+        }
+        if(distance>8){
+            dis_price = 6+(8-2)*0.8+(distance-8)*1.2
+        }
+        totalPrice = Math.round(dis_price+time_price);
+        result = '============= 价格明细 =============\n'
+        result += '总计：' + totalPrice.toString() +'元\n';
+        result += '===================================';
+        return result;
+    }
+    var result = getPrice(Input);
+    console.log(result);
+    return result;
+}
+module.exports=Charge;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,41 @@
-const main = require('../main/main');
+var Charge = require('../main/main.js');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it('should generate charge journey is : 两公里以内', function() {
+        let inputs = [1.5, 0];
+        let summary = Charge(inputs).trim();
+        let expected = `
+============= 价格明细 =============
+总计：6元
+===================================`.trim()
+        expect(summary).toEqual(expected)
+    });
+    it('should generate charge journey is : 八公里以内', function() {
+        let inputs = [6.5,0];
+        let summary = Charge(inputs).trim();
+        let expected = `
+============= 价格明细 =============
+总计：10元
+===================================`.trim()
+        expect(summary).toEqual(expected)
+    });
+    it('should generate charge journey is : 八公里以上', function() {
+        let inputs = [10, 0];
+        let summary = Charge(inputs).trim();
+        let expected = `
+============= 价格明细 =============
+总计：13元
+===================================`.trim()
+        expect(summary).toEqual(expected)
+    });
+    it('should generate charge journey is : 含等待时间', function() {
+        let inputs = [10, 5];
+        let summary = Charge(inputs).trim();
+        let expected = `
+============= 价格明细 =============
+总计：14元
+===================================`.trim()
+        expect(summary).toEqual(expected)
+    });
 });
